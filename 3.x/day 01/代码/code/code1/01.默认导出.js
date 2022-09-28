d15cfa6ed7f2ff10473d8b230f4b1c34a3f3b5e0
xubin24 <1327060448@qq.com>
2022-09-28 22:30:14 +0800

    添加vue3内容

---
+++ 3.x/day 01/代码/code/code1/01.默认导出.js
@@ -0,0 +1,17 @@
+let n1 = 10
+export let n2 = 20
+let n3 = 30;
+function show() {}
+
+export default {
+  n1,
+  show
+}
+
+export {
+	n3
+}
+
+// export default {
+//   n2
+// }
---
+++ 3.x/day 01/代码/code/code1/02.默认导入.js
@@ -0,0 +1,4 @@
+import m1 from './01.默认导出.js'
+import * as m2 from './01.默认导出.js'
+console.log(m1)
+console.log(m2)
